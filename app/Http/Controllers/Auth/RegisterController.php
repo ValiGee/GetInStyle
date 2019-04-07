2edf814c0ce68656488549b30b63e3703e182c1e
iandu <--global>
2019-04-07 19:43:53 +0300

    Add user avatar

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,6 +53,7 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'avatar' => ['file', 'image', 'max:10240'],
         ]);
     }
 
@@ -73,10 +74,16 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+
+        if (isset($data['avatar'])) {
+            $user->update(['avatar' => 'storage/' . $data['avatar']->store('users_avatar', 'public')]);
+        }
+
+        return $user;
     }
 }
